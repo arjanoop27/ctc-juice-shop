38d3270a5992924b3378af5b2cc057f1e14d1400
Anoop RJ <arjanoop@gmail.com>
2026-01-19 09:50:30 +0100

    WIP: Add models for CTC

---
+++ frontend/src/app/crack-the-code/models/index.ts
@@ -0,0 +1,3 @@
+export {default as RegisterUserRequest} from './../models/registerUserRequest';
+export {default as LoginUserRequest} from './../models/loginUserRequest';
+export {default as LoginSuccessResponse} from './../models/loginSuccessResponse';
