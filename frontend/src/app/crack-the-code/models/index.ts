6a957e5249f8969002579e032a2d8bab4b588ad7
Anoop RJ <arjanoop@gmail.com>
2026-01-20 22:35:14 +0100

    WIP: Add ctcChallenge model

--- frontend/src/app/crack-the-code/models/index.ts
+++ frontend/src/app/crack-the-code/models/index.ts
@@ -4,4 +4,5 @@ export {default as RegisterUserRequest} from './../models/registerUserRequest';
 export {default as LoginUserRequest} from './../models/loginUserRequest';
 export {default as LoginSuccessResponse} from './../models/loginSuccessResponse';
 export {default as UserDetail} from './../models/userDetail';
+export {default as CtcChallenge} from './../models/ctcChallenge';
 
