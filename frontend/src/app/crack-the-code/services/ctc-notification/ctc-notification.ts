b01299d376bd6f4b40ffd9aa8e75aa82a64843f2
Anoop RJ <arjanoop@gmail.com>
2026-02-05 12:48:42 +0100

    WIP: Refactor Notification Service

--- frontend/src/app/crack-the-code/services/ctc-notification/ctc-notification.ts
+++ frontend/src/app/crack-the-code/services/ctc-notification/ctc-notification.ts
@@ -3,44 +3,46 @@ import {io, Socket} from 'socket.io-client';
 import {environment} from "../../../../environments/environment";
 import {CtcCurrentSelection} from "../ctc-current-selection/ctc-current-selection";
 import {CtcNarrativeSelection} from "../ctc-narrative-selection/ctc-narrative-selection";
+import {CtcNotificationHelper} from "../ctc-notification-helper/ctc-notification-helper";
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class CtcNotification {
-  private socket?: Socket;
-  private readonly ctcBffServer = environment.ctcBffServer
-  private readonly currentChallenge = inject(CtcCurrentSelection)
-  private readonly currentNarrative = inject(CtcNarrativeSelection)
+    private socket?: Socket;
+    private readonly ctcBffServer = environment.ctcBffServer
+    private readonly currentChallenge = inject(CtcCurrentSelection)
+    private readonly currentNarrative = inject(CtcNarrativeSelection)
+    private readonly notificationHelper = inject(CtcNotificationHelper)
 
-  connect(accessToken?: string) {
+    connect(accessToken?: string) {
 
-    this.socket = io(this.ctcBffServer, {
-      transports: ['websocket'],
-      withCredentials: true,
-      auth: accessToken ? {token: accessToken} : undefined,
-    });
+        this.socket = io(this.ctcBffServer, {
+            transports: ['websocket'],
+            withCredentials: true,
+            auth: accessToken ? {token: accessToken} : undefined,
+        });
 
-    this.socket.on('connect', () => {
-      console.log('ws connected', this.socket?.id);
-    });
+        this.socket.on('connect', () => {
+            console.log('WS connected');
+        });
 
-    this.socket.on('challengeSolved', (msg) => {
-      console.log('challengeSolved', msg);
-      import('../../../../confetti').then(module => {
-        module.shootConfetti()
-      })
-      this.currentChallenge.clear();
-      this.currentNarrative.clear();
-    });
+        this.socket.on('challengeSolved', (msg) => {
+            this.notificationHelper.open('Challenge Solved!');
+            import('../../../../confetti').then(module => {
+                module.shootConfetti()
+            })
+            this.currentChallenge.clear();
+            this.currentNarrative.clear();
+        });
 
-    this.socket.on('connect_error', (err) => {
-      console.error('ws connect_error', err.message);
-    });
-  }
+        this.socket.on('connect_error', (err) => {
+            console.error('ws connect_error', err.message);
+        });
+    }
 
-  disconnect() {
-    this.socket?.disconnect();
-    this.socket = undefined;
-  }
+    disconnect() {
+        this.socket?.disconnect();
+        this.socket = undefined;
+    }
 }
